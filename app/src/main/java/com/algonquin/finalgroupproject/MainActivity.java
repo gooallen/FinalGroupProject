b69d7560609a50a7fcc6309d11d5097b5e3a6ed7
fu000045 <fu000045@algonquinlive.com>
2018-03-19 10:57:17 -0400

    Verison_with toolbar

--- app/src/main/java/com/algonquin/finalgroupproject/MainActivity.java
+++ app/src/main/java/com/algonquin/finalgroupproject/MainActivity.java
@@ -1,27 +1,54 @@
 package com.algonquin.finalgroupproject;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn_quizCreater;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_quizCreater = findViewById(R.id.button_quizCreater);
-        btn_quizCreater.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Jump to QuizCreaterActivity.
-                Intent intent = new Intent(MainActivity.this, QuizCreatorActivity.class);
-                startActivity(intent);
-            }
-        });
+        toolbar = findViewById(R.id.toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.Quiz:
+                item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, "Quiz Creater Selected!", Toast.LENGTH_SHORT).show();
+                        //Jump to QuizCreaterActivity.
+                        Intent intent = new Intent(MainActivity.this, QuizCreatorActivity.class);
+                        startActivity(intent);
+                        return true;
+                    }
+                });
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/algonquin/finalgroupproject/QuizCreatorActivity.java
+++ app/src/main/java/com/algonquin/finalgroupproject/QuizCreatorActivity.java
@@ -1,13 +1,62 @@
 package com.algonquin.finalgroupproject;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class QuizCreatorActivity extends Activity {
+
+    LinearLayout linearlayout;
+    Button btn_createAQuiz;
+    Button btn_quizPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_creator);
+
+        linearlayout = findViewById(R.id.linearlayout);
+        btn_createAQuiz = findViewById(R.id.Create_Quiz);
+        btn_quizPool = findViewById(R.id.View_Quiz_Pool);
+
+        //From: Android Snackbar Example Tutorial [Web Page]
+        //Retrieved from: https://www.journaldev.com/10324/android-snackbar-example-tutorial
+        btn_createAQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar
+                        .make(linearlayout, "Create a quiz from the pool!", Snackbar.LENGTH_LONG);
+//                        .setAction("RETRY", new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                            }
+//                        });
+//                snackbar.setActionTextColor(Color.RED);
+//                View sbView = snackbar.getView();
+//                TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+//                textView.setTextColor(Color.YELLOW);
+                snackbar.show();
+
+//                //Jump to ViewQuizPool.
+//                Intent intent = new Intent(QuizCreatorActivity.this, CreateAQuiz.class);
+//                startActivity(intent);
+            }
+        });
+
+//        btn_quizPool.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //Jump to ViewQuizPool.
+//                Intent intent = new Intent(QuizCreatorActivity.this, ViewQuizPool.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
